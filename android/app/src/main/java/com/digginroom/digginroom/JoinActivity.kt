59407a9c70af9aa6b7dee392aaae7f6d0d0578c6
woogi <kimjinuk1999@gmail.com>
2023-07-12 23:40:02 +0900

    feat: 회원가입 ui 구성

---
+++ android/app/src/main/java/com/digginroom/digginroom/JoinActivity.kt
@@ -0,0 +1,11 @@
+package com.digginroom.digginroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
