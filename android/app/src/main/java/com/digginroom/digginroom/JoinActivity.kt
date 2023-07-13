71ca6ba2c0a10c057b8588a761f973ff9f1176b0
woogi <kimjinuk1999@gmail.com>
2023-07-13 15:16:02 +0900

    feat: id와 password를 입력하고 회원가입 버튼을 누르면 다른 화면으로 이동한다

--- android/app/src/main/java/com/digginroom/digginroom/JoinActivity.kt
+++ android/app/src/main/java/com/digginroom/digginroom/JoinActivity.kt
@@ -1,11 +1,61 @@
 package com.digginroom.digginroom
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.digginroom.digginroom.databinding.ActivityJoinBinding
+import com.digginroom.model.join.JoinState
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityJoinBinding
+    private val joinViewModel: JoinViewModel by lazy {
+        ViewModelProvider(
+            this,
+            JoinViewModel.getJoinViewModelFactory()
+        )[JoinViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+        initJoinObserver()
+        initJoinButtonListener()
+    }
+
+    private fun initJoinObserver() {
+        joinViewModel.state.observe(this) { joinState ->
+            when (joinState) {
+                is JoinState.Success -> navigateToRoomView()
+                is JoinState.Retry -> clearInputIdAndPassword()
+            }
+        }
+    }
+
+    private fun initJoinButtonListener() {
+        with(binding) {
+            joinButtonJoin.setOnClickListener {
+                joinViewModel.join(
+                    id = joinTvId.text.toString(),
+                    password = joinTvPassword.text.toString()
+                )
+            }
+        }
+    }
+
+    private fun navigateToRoomView() {
+        // todo: RoomView가 구성되어 있지 않은 상황에서 임시로 구현
+        val intent = MainActivity.getIntent(this)
+
+        startActivity(intent)
+    }
+
+    private fun clearInputIdAndPassword() {
+        with(binding) {
+            joinEtInputId.text.clear()
+            joinEtInputPassword.text.clear()
+        }
     }
 }
--- android/app/src/main/java/com/digginroom/digginroom/MainActivity.kt
+++ android/app/src/main/java/com/digginroom/digginroom/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.digginroom.digginroom
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+
+        fun getIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
     }
 }
