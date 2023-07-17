567603f4111eeddfc9fd58c4372661344aa78bbb
woogi <kimjinuk1999@gmail.com>
2023-07-17 20:03:25 +0900

    feat: 로그인 뷰를 구성한다

---
+++ android/app/src/main/java/com/digginroom/digginroom/views/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.digginroom.digginroom.views.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.digginroom.digginroom.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
