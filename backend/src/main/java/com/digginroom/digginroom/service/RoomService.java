c033bd14eee045d0ce8893bcf04642e6d0955880
songusika <sws981202@gmail.com>
2023-07-17 19:27:03 +0900

    feat: 랜덤으로 룸 선택

---
+++ backend/src/main/java/com/digginroom/digginroom/service/RoomService.java
@@ -0,0 +1,40 @@
+package com.digginroom.digginroom.service;
+
+import com.digginroom.digginroom.domain.Room;
+import com.digginroom.digginroom.repository.RoomRepository;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    @Transactional
+    public Room pickRandom() {
+        List<Room> rooms = roomRepository.findAll();
+
+        Collections.shuffle(rooms);
+
+        return rooms.get(0);
+    }
+
+    @Transactional
+    public Room pickRandomByPage() {
+        int count = Long.valueOf(roomRepository.count()).intValue();
+
+        Pageable pageable = Pageable.ofSize(1)
+                .withPage(ThreadLocalRandom.current().nextInt(count));
+
+        Page<Room> randomizedPage = roomRepository.findAll(pageable);
+
+        return randomizedPage.getContent().get(0);
+    }
+}
---
+++ backend/src/test/java/com/digginroom/digginroom/service/RoomServiceTest.java
@@ -0,0 +1,52 @@
+package com.digginroom.digginroom.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.digginroom.digginroom.domain.MediaSource;
+import com.digginroom.digginroom.domain.MediaType;
+import com.digginroom.digginroom.domain.Room;
+import com.digginroom.digginroom.repository.RoomRepository;
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+class RoomServiceTest {
+
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private RoomService roomService;
+
+    @Test
+    void 여러_룸_중_랜덤으로_하나를_선택한다() {
+        Room 나무 = new Room(List.of(new MediaSource(MediaType.YOUTUBE, "lQcnNPqy2Ww")));
+        Room 가까운듯먼그대여 = new Room(List.of(new MediaSource(MediaType.YOUTUBE, "2VkWaOOF4Rc")));
+        roomRepository.save(나무);
+        roomRepository.save(가까운듯먼그대여);
+
+        final var pickedRoom = roomService.pickRandom();
+
+        assertThat(pickedRoom).isNotNull();
+    }
+
+    @Test
+    void 페이징을_사용하여_여러_룸_중_랜덤으로_하나를_선택한다() {
+        Room 나무 = new Room(List.of(new MediaSource(MediaType.YOUTUBE, "lQcnNPqy2Ww")));
+        Room 가까운듯먼그대여 = new Room(List.of(new MediaSource(MediaType.YOUTUBE, "2VkWaOOF4Rc")));
+        roomRepository.save(나무);
+        roomRepository.save(가까운듯먼그대여);
+
+        final var pickedRoom = roomService.pickRandomByPage();
+
+        assertThat(pickedRoom).isNotNull();
+    }
+}
