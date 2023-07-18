648b8813f72d701bef10dbb48a8e4a9b4db079a5
songusika <sws981202@gmail.com>
2023-07-18 13:15:58 +0900

    feat: 룸 탐색 컨트롤러 구현

---
+++ backend/src/main/java/com/digginroom/digginroom/controller/RoomController.java
@@ -0,0 +1,19 @@
+package com.digginroom.digginroom.controller;
+
+import com.digginroom.digginroom.controller.dto.RoomResponse;
+import com.digginroom.digginroom.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @GetMapping("/room")
+    public RoomResponse showRandomRoom() {
+        return roomService.pickRandom();
+    }
+}
