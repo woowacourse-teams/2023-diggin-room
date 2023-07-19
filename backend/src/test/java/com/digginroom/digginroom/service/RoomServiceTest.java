ee667ea367c145336c885f9ecc83b990977a96ba
0chil <gorae02@gmail.com>
2023-07-19 17:18:01 +0900

    feat: 룸 스크랩, 취소를 구현한다 (#57)
    * feat: 미디어 소스 구현
    
    * feat: 룸, 미디어 소스 엔티티 구현
    
    * feat: 룸 연관관계 편의 메서드
    
    * feat: 랜덤으로 룸 선택
    
    * refactor: 룸 연관관계 수정
    
    * feat: 룸 탐색 컨트롤러 구현
    
    * feat: 룸 없음 예외처리
    
    * style: 핸들러 리포맷팅
    
    * refactor: Transactional readOnly 적용
    
    * feat: 룸 스크랩, 취소, 룸 스크랩 여부 구현
    
    * chore: 사용하지 않는 클래스 삭제
    
    * refactor: hasScrapped 재사용
    
    * feat: 스크랩 예외 처리
    
    ---------
    
    Co-authored-by: songusika <sws981202@gmail.com>

--- backend/src/test/java/com/digginroom/digginroom/service/RoomServiceTest.java
+++ backend/src/test/java/com/digginroom/digginroom/service/RoomServiceTest.java
@@ -3,7 +3,9 @@ package com.digginroom.digginroom.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.digginroom.digginroom.domain.MediaSource;
+import com.digginroom.digginroom.domain.Member;
 import com.digginroom.digginroom.domain.Room;
+import com.digginroom.digginroom.repository.MemberRepository;
 import com.digginroom.digginroom.repository.RoomRepository;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -23,16 +25,66 @@ class RoomServiceTest {
     private RoomRepository roomRepository;
     @Autowired
     private RoomService roomService;
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     void 페이징을_사용하여_여러_룸_중_랜덤으로_하나를_선택한다() {
+        Member member = memberRepository.save(new Member("member", "1234"));
         Room 나무 = new Room(new MediaSource("lQcnNPqy2Ww"));
         Room 가까운듯먼그대여 = new Room(new MediaSource("2VkWaOOF4Rc"));
         roomRepository.save(나무);
         roomRepository.save(가까운듯먼그대여);
 
-        final var pickedRoom = roomService.pickRandom();
+        final var pickedRoom = roomService.pickRandom(member.getId());
 
         assertThat(pickedRoom).isNotNull();
+    }
+
+    @Test
+    void 멤버는_룸을_스크랩할_수_있다() {
+        Member member = memberRepository.save(new Member("member", "1234"));
+        Room room = roomRepository.save(new Room(new MediaSource("lQcnNPqy2Ww")));
+
+        roomService.scrap(member.getId(), room.getId());
+
+        assertThat(member.getScraps()).contains(room);
+    }
+
+    @Test
+    void 이미_스크랩된_룸은_다시_스크랩되지_않는다() {
+        Member member = memberRepository.save(new Member("member", "1234"));
+        Room room = roomRepository.save(new Room(new MediaSource("lQcnNPqy2Ww")));
+
+        roomService.scrap(member.getId(), room.getId());
+        roomService.scrap(member.getId(), room.getId());
+
+        assertThat(member.getScraps()).hasSize(1);
+    }
+
+    @Test
+    void 멤버는_룸스크랩을_취소할_수_있다() {
+        Member member = memberRepository.save(new Member("member", "1234"));
+        Room room = roomRepository.save(new Room(new MediaSource("lQcnNPqy2Ww")));
+        roomService.scrap(member.getId(), room.getId());
+
+        roomService.unscrap(member.getId(), room.getId());
+
+        assertThat(member.getScraps()).isEmpty();
+    }
+
+    @Test
+    void 이미_스크랩_취소된_룸은_다시_취소되지_않는다() {
+        Member member = memberRepository.save(new Member("member", "1234"));
+        Room room = roomRepository.save(new Room(new MediaSource("lQcnNPqy2Ww")));
+        Room otherRoom = roomRepository.save(new Room(new MediaSource("lQcnNPqy3Ww")));
+
+        roomService.scrap(member.getId(), room.getId());
+        roomService.scrap(member.getId(), otherRoom.getId());
+
+        roomService.unscrap(member.getId(), room.getId());
+        roomService.unscrap(member.getId(), room.getId());
+
+        assertThat(member.getScraps()).hasSize(1);
     }
 }
