9a11c2478e763e1ef0c81dbf0fee7fb85c6ddf45
thdwoqor <celebarte01@gmail.com>
2023-07-19 14:35:41 +0900

    style: 구글 컨벤션 적용

--- backend/src/main/java/com/digginroom/digginroom/controller/MemberController.java
+++ backend/src/main/java/com/digginroom/digginroom/controller/MemberController.java
@@ -7,7 +7,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/join")
