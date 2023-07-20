95a7a496228cbf46ef1ba2495053484f632aaa2c
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-07-20 14:10:13 +0900

    feat: dto validation 예외 핸들링 구현

--- backend/src/main/java/com/digginroom/digginroom/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/digginroom/digginroom/controller/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.digginroom.digginroom.controller;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import com.digginroom.digginroom.controller.dto.ErrorResponse;
 import com.digginroom.digginroom.exception.DigginRoomException;
 import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -29,5 +32,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(new ErrorResponse(LocalDateTime.now(), e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        log.info(e.getMessage());
+
+        String message = e.getFieldErrors().stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("");
+
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(new ErrorResponse(LocalDateTime.now(), message));
     }
 }
