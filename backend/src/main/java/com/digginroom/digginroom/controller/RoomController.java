d277d0e207777275bcd6d28381ff123fafc379a1
0chil <gorae02@gmail.com>
2023-07-20 14:49:26 +0900

    feat: 룸 스크랩, 취소 엔드포인트 (#71)
    * feat: 룸 스크랩, 취소 엔드포인트
    
    * style: 임포트 순서 복구
    
    * typo: final 누락

--- backend/src/main/java/com/digginroom/digginroom/controller/RoomController.java
+++ backend/src/main/java/com/digginroom/digginroom/controller/RoomController.java
@@ -1,9 +1,15 @@
 package com.digginroom.digginroom.controller;
 
+import com.digginroom.digginroom.controller.dto.RoomRequest;
 import com.digginroom.digginroom.controller.dto.RoomResponse;
 import com.digginroom.digginroom.service.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +21,16 @@ public class RoomController {
     @GetMapping("/room")
     public RoomResponse showRandomRoom(@Auth final Long memberId) {
         return roomService.pickRandom(memberId);
+    }
+
+    @PostMapping("/scrap")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void scrap(@Auth final Long memberId, @RequestBody final RoomRequest roomRequest) {
+        roomService.scrap(memberId, roomRequest.roomId());
+    }
+
+    @DeleteMapping("/scrap")
+    public void showRandomRoom(@Auth final Long memberId, @RequestBody final RoomRequest roomRequest) {
+        roomService.unscrap(memberId, roomRequest.roomId());
     }
 }
