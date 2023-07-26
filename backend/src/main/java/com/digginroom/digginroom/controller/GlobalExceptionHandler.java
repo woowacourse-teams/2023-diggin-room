b6d1b03bab3f76e5ac024993cafa6219a70ff4d3
songusika <sws981202@gmail.com>
2023-07-26 14:57:35 +0900

    feat: 세부 예외 처리 구현

--- backend/src/main/java/com/digginroom/digginroom/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/digginroom/digginroom/controller/GlobalExceptionHandler.java
@@ -11,7 +11,13 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -20,7 +26,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler({Exception.class, BindException.class})
     public ResponseEntity<ErrorResponse> handleUnexpectedException(final Exception e) {
         log.error(e.getMessage(), e);
 
@@ -56,5 +62,46 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(BAD_REQUEST)
                 .body(new ErrorResponse(LocalDateTime.now(), message));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupported(
+            final HttpRequestMethodNotSupportedException e
+    ) {
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(new ErrorResponse(LocalDateTime.now(), "HTTP 메서드를 확인해주세요."));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException() {
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(new ErrorResponse(LocalDateTime.now(), "요청 페이로드를 확인해주세요."));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
+            final MissingServletRequestParameterException e
+    ) {
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(new ErrorResponse(LocalDateTime.now(), "쿼리 스트링을 확인해주세요."));
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(
+            final HttpMediaTypeNotSupportedException e
+    ) {
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(new ErrorResponse(LocalDateTime.now(), "미디어 타입을 확인해주세요."));
+    }
+
+    @ExceptionHandler(MissingPathVariableException.class)
+    public ResponseEntity<ErrorResponse> handleMissingPathVariableException(final MissingPathVariableException e) {
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(new ErrorResponse(LocalDateTime.now(), "경로 매개변수 확인해주세요."));
     }
 }
