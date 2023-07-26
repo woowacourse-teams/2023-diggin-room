3828d7a46e6f2607e748592251b55fa92425f02d
songusika <sws981202@gmail.com>
2023-07-26 14:29:57 +0900

    refactor: 서버 에러만 에러 로깅하도록 수정 및 메서드명 통일

--- backend/src/main/java/com/digginroom/digginroom/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/digginroom/digginroom/controller/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.digginroom.digginroom.controller;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import com.digginroom.digginroom.controller.dto.ErrorResponse;
 import com.digginroom.digginroom.exception.DigginRoomException;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
@@ -19,15 +21,17 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<ErrorResponse> noHandlerFoundException() {
+    public ResponseEntity<ErrorResponse> handleNoHandlerFoundException() {
         return ResponseEntity
                 .status(NOT_FOUND)
                 .body(new ErrorResponse(LocalDateTime.now(), "리소스를 찾을 수 없습니다."));
     }
 
     @ExceptionHandler(DigginRoomException.class)
-    public ResponseEntity<ErrorResponse> digginRoomException(final DigginRoomException e) {
-        log.info(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleDigginRoomException(final DigginRoomException e) {
+        if (e.getHttpStatus() == INTERNAL_SERVER_ERROR) {
+            log.error(e.getMessage());
+        }
 
         return ResponseEntity
                 .status(e.getHttpStatus())
@@ -36,12 +40,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
-        log.info(e.getMessage());
-
         String message = e.getFieldErrors().stream()
-                .findFirst()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .orElse("");
+                .collect(Collectors.joining(", "));
 
         return ResponseEntity
                 .status(BAD_REQUEST)
