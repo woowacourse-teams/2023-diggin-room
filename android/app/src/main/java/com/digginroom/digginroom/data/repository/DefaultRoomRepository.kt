359dc5deb5f25806dae6c80d6db04446a094b1cf
woogi <kimjinuk1999@gmail.com>
2023-07-27 21:34:25 +0900

    feat: 스크랩된 룸 목록 데이터를 불러올 때 데이터 소스에서 가짜 데이터를 반환한다.

--- android/app/src/main/java/com/digginroom/digginroom/data/repository/DefaultRoomRepository.kt
+++ android/app/src/main/java/com/digginroom/digginroom/data/repository/DefaultRoomRepository.kt
@@ -2,9 +2,7 @@ package com.digginroom.digginroom.data.repository
 
 import com.digginroom.digginroom.data.datasource.local.TokenLocalDataSource
 import com.digginroom.digginroom.data.datasource.remote.RoomRemoteDataSource
-import com.digginroom.digginroom.model.room.Genre
 import com.digginroom.digginroom.model.room.Room
-import com.digginroom.digginroom.model.room.Song
 import com.digginroom.digginroom.repository.RoomRepository
 import com.digginroom.digginroom.views.model.mapper.RoomMapper.toDomain
 import kotlinx.coroutines.CoroutineDispatcher
@@ -16,6 +14,7 @@ class DefaultRoomRepository(
     private val tokenLocalDataSource: TokenLocalDataSource,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : RoomRepository {
+
     override suspend fun findNext(): Result<Room> {
         return withContext(ioDispatcher) {
             runCatching {
@@ -24,24 +23,12 @@ class DefaultRoomRepository(
         }
     }
 
-    override fun findScrapped(): List<Room> {
-        return listOf(
-            Room(
-                videoId = "pOviO3wKfV0",
-                song = Song(
-                    title = "예시 제목",
-                    albumTitle = "예시 앨범 타이틀",
-                    artist = "예시 아티스트",
-                    genres = List(3) {
-                        Genre("예시 장르$it")
-                    },
-                    tags = List(5) {
-                        "예시 태그$it"
-                    }
-                ),
-                isScrapped = true
-            )
-        )
+    override suspend fun findScrapped(): Result<List<Room>> {
+        return withContext(ioDispatcher) {
+            runCatching {
+                roomRemoteDataSource.findScrapped()
+            }
+        }
     }
 
     override fun updateScrapById(id: Long, value: Boolean) {
--- android/app/src/main/java/com/digginroom/digginroom/viewmodels/ScrapViewModel.kt
+++ android/app/src/main/java/com/digginroom/digginroom/viewmodels/ScrapViewModel.kt
@@ -3,19 +3,36 @@ package com.digginroom.digginroom.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.digginroom.digginroom.model.room.Room
 import com.digginroom.digginroom.repository.RoomRepository
+import com.digginroom.digginroom.views.model.RoomModel
+import com.digginroom.digginroom.views.model.mapper.RoomMapper.toModel
+import kotlinx.coroutines.launch
 
 class ScrapViewModel(
-    private val rooms: MutableList<Room> = mutableListOf(),
+    private var rooms: MutableList<Room>,
     private val roomRepository: RoomRepository
 ) : ViewModel() {
 
-    private val _scrappedRooms: MutableLiveData<List<Room>> = MutableLiveData(rooms)
-    private val scrappedRooms: LiveData<List<Room>>
+    private val _scrappedRooms: MutableLiveData<List<RoomModel>> = MutableLiveData(listOf())
+    val scrappedRooms: LiveData<List<RoomModel>>
         get() = _scrappedRooms
 
+    init {
+        findScrappedRooms()
+    }
+
     fun findScrappedRooms() {
-        _scrappedRooms.value = roomRepository.findScrapped()
+        viewModelScope.launch {
+            roomRepository.findScrapped()
+                .onSuccess {
+                    rooms.addAll(it)
+                    _scrappedRooms.value = rooms.map { room ->
+                        room.toModel()
+                    }
+                }.onFailure {
+                }
+        }
     }
 }
