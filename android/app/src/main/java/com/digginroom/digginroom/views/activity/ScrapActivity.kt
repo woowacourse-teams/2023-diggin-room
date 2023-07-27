42e7db205d5862daf2e7e50b67fe773aea4d925f
woogi <kimjinuk1999@gmail.com>
2023-07-27 13:34:55 +0900

    feat: 스크랩된 룸 목록을 볼 수 있는 화면을 구성한다

---
+++ android/app/src/main/java/com/digginroom/digginroom/views/activity/ScrapActivity.kt
@@ -0,0 +1,12 @@
+package com.digginroom.digginroom.views.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.digginroom.digginroom.R
+
+class ScrapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrap)
+    }
+}
