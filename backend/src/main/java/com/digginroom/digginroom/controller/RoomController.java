5826923c067ad149e44189b402629d0595844ebc
songusika <sws981202@gmail.com>
2023-07-28 16:57:24 +0900

    refactor: RoomController의 반환 타입 통일

--- backend/src/main/java/com/digginroom/digginroom/controller/RoomController.java
+++ backend/src/main/java/com/digginroom/digginroom/controller/RoomController.java
@@ -5,11 +5,11 @@ import com.digginroom.digginroom.controller.dto.RoomResponse;
 import com.digginroom.digginroom.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,18 +19,19 @@ public class RoomController {
     private final RoomService roomService;
 
     @GetMapping("/room")
-    public RoomResponse showRandomRoom(@Auth final Long memberId) {
-        return roomService.pickRandom(memberId);
+    public ResponseEntity<RoomResponse> showRandomRoom(@Auth final Long memberId) {
+        return ResponseEntity.ok().body(roomService.pickRandom(memberId));
     }
 
     @PostMapping("/scrap")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void scrap(@Auth final Long memberId, @RequestBody final RoomRequest roomRequest) {
+    public ResponseEntity<Void> scrap(@Auth final Long memberId, @RequestBody final RoomRequest roomRequest) {
         roomService.scrap(memberId, roomRequest.roomId());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/scrap")
-    public void showRandomRoom(@Auth final Long memberId, @RequestBody final RoomRequest roomRequest) {
+    public ResponseEntity<Void> showRandomRoom(@Auth final Long memberId, @RequestBody final RoomRequest roomRequest) {
         roomService.unscrap(memberId, roomRequest.roomId());
+        return ResponseEntity.ok().build();
     }
 }
