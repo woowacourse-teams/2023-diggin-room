e282b2c4d60952d093c4273baffbd79693800c0e
woogi <kimjinuk1999@gmail.com>
2023-07-28 16:32:50 +0900

    feat: LoginViewModel에 대한 테스트를 작성한다

---
+++ android/app/src/test/java/com/digginroom/digginroom/LoginViewModelTest.kt
@@ -0,0 +1,116 @@
+package com.digginroom.digginroom
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.digginroom.digginroom.repository.AccountRepository
+import com.digginroom.digginroom.repository.TokenRepository
+import com.digginroom.digginroom.viewmodels.LoginViewModel
+import com.digginroom.digginroom.views.activity.LoginState
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class LoginViewModelTest {
+
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var accountRepository: AccountRepository
+    private lateinit var tokenRepository: TokenRepository
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+
+        accountRepository = mockk(relaxed = true)
+        tokenRepository = mockk(relaxed = true)
+
+        loginViewModel = LoginViewModel(
+            accountRepository = accountRepository,
+            tokenRepository = tokenRepository
+        )
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `로그인 실패시 로그인 상태를 나타내는 변수를 failed로 바꾼다`() {
+        // given
+        coEvery {
+            accountRepository.postLogIn(
+                id = EXAMPLE_ID,
+                password = EXAMPLE_PASSWORD
+            )
+        } returns Result.failure(Throwable())
+
+        loginViewModel.id.value = EXAMPLE_ID
+        loginViewModel.password.value = EXAMPLE_PASSWORD
+
+        // when
+        loginViewModel.login()
+
+        // then
+        assertEquals(LoginState.FAILED, loginViewModel.state.value)
+    }
+
+    @Test
+    fun `로그인 성공시 로그인 상태를 나타내는 변수를 sucess로 바꾼다`() {
+        // given
+        coEvery {
+            accountRepository.postLogIn(
+                id = EXAMPLE_ID,
+                password = EXAMPLE_PASSWORD
+            )
+        } returns Result.success(TOKEN)
+
+        loginViewModel.id.value = EXAMPLE_ID
+        loginViewModel.password.value = EXAMPLE_PASSWORD
+
+        // when
+        loginViewModel.login()
+
+        // then
+        assertEquals(LoginState.SUCCEED, loginViewModel.state.value)
+    }
+
+    @Test
+    fun `로그인 성공시 로그인 상태를 나타내는 token값을 토큰 저장소에 저장한다`() {
+        // given
+        coEvery {
+            accountRepository.postLogIn(
+                id = EXAMPLE_ID,
+                password = EXAMPLE_PASSWORD
+            )
+        } returns Result.success(TOKEN)
+
+        loginViewModel.id.value = EXAMPLE_ID
+        loginViewModel.password.value = EXAMPLE_PASSWORD
+
+        // when
+        loginViewModel.login()
+
+        // then
+        verify { tokenRepository.save(TOKEN) }
+    }
+
+    companion object {
+
+        private const val EXAMPLE_ID = "digginroom"
+        private const val EXAMPLE_PASSWORD = "digginroom"
+        private const val TOKEN = ""
+    }
+}
