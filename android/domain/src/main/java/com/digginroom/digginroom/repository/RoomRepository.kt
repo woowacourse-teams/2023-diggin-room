d86d5a7b7d09320e4bd1275744c8b169c6419065
Doyeop Kim <dygames34@gmail.com>
2023-08-01 14:37:22 +0900

    feat: Logger와 LogResult 구현 (#137)

--- android/domain/src/main/java/com/digginroom/digginroom/repository/RoomRepository.kt
+++ android/domain/src/main/java/com/digginroom/digginroom/repository/RoomRepository.kt
@@ -1,10 +1,11 @@
 package com.digginroom.digginroom.repository
 
+import com.digginroom.digginroom.logging.LogResult
 import com.digginroom.digginroom.model.room.Room
 
 interface RoomRepository {
 
-    suspend fun findNext(): Result<Room>
+    suspend fun findNext(): LogResult<Room>
     fun updateScrapById(id: Long, value: Boolean)
     fun updateWeightById(id: Long, value: Double)
 }
