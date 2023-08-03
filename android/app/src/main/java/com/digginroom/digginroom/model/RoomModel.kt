4691dee6e9427355b26e7fb841a23a94c08124b9
woogi <kimjinuk1999@gmail.com>
2023-08-03 11:00:11 +0900

    refactor: RoomModel을 ScrapRoomActivity로 전달하기 위한 클래스인 RoomModels 클래스 생성

--- android/app/src/main/java/com/digginroom/digginroom/model/RoomModel.kt
+++ android/app/src/main/java/com/digginroom/digginroom/model/RoomModel.kt
@@ -1,7 +1,9 @@
 package com.digginroom.digginroom.model
 
+import java.io.Serializable
+
 data class RoomModel(
     val videoId: String,
     val song: SongModel,
     val isScrapped: Boolean
-)
+) : Serializable
--- android/app/src/main/java/com/digginroom/digginroom/model/SongModel.kt
+++ android/app/src/main/java/com/digginroom/digginroom/model/SongModel.kt
@@ -1,9 +1,11 @@
 package com.digginroom.digginroom.model
 
+import java.io.Serializable
+
 data class SongModel(
     val title: String,
     val albumTitle: String,
     val artist: String,
     val genres: List<GenreModel>,
     val tags: List<String>
-)
+) : Serializable
