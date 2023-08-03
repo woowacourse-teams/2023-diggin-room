551fb0b8cf6e473d042696464276e6dbe0f90788
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2023-08-03 22:45:58 +0900

    feat: YoutubeRoomPlayer 에 스크랩 뷰 구현 (#151)
    * remove: 불필요한 이미지 삭제
    
    * remove: 불필요한 item_room_pager.xml 파일 삭제
    
    * feat: YoutubeRoomPlayer 에 스크랩 아이콘과 음악 정보 레이아웃 추가
    
    * feat: 스크랩, 음악정보 커스텀 레이아웃 생성 및 커스텀 속성 생성
    
    * feat: 음악정보 커스텀뷰의 자식으로 스크랩 커스텀뷰를 갖도록 구현
    
    * style: ktlint 적용
    
    * feat: 스크랩 버튼에 뷰모델의 서버 요청 리스너 연결
    
    * feat: RoomRecycler 파일의 roomViewModel 의존성 제거
    
    * feat: 스크랩 취소 repository 메소드 작성
    
    * feat: RoomInfo 스크랩 버튼에 서버 통신 로직 연결

--- android/app/src/main/java/com/digginroom/digginroom/data/datasource/remote/RoomRemoteDataSource.kt
+++ android/app/src/main/java/com/digginroom/digginroom/data/datasource/remote/RoomRemoteDataSource.kt
@@ -2,7 +2,6 @@ package com.digginroom.digginroom.data.datasource.remote
 
 import com.digginroom.digginroom.data.entity.CancelScrapErrorResponse
 import com.digginroom.digginroom.data.entity.CancelScrapRequest
-import com.digginroom.digginroom.data.entity.JoinErrorResponse
 import com.digginroom.digginroom.data.entity.RoomErrorResponse
 import com.digginroom.digginroom.data.entity.RoomResponse
 import com.digginroom.digginroom.data.entity.ScrapErrorResponse
@@ -18,9 +17,9 @@ class RoomRemoteDataSource(
         val response: Response<RoomResponse> = roomService.findNext(cookie)
 
         if (response.isSuccessful) {
-            return response.body() ?: throw JoinErrorResponse.Default()
+            return response.body() ?: throw RoomErrorResponse.Default()
         }
-        throw JoinErrorResponse.Default()
+        throw RoomErrorResponse.Default()
     }
 
     suspend fun findScrapped(cookie: String): ScrappedRoomsResponse {
