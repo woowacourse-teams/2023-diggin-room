cae4524642189dccb739b66d6485236f6f722acb
woogi <kimjinuk1999@gmail.com>
2023-08-03 15:17:31 +0900

    refactor: RoomFixture에서 제공하는 Room 가짜 데이터에 Track 데이터를 삽입한다.

--- android/app/src/test/java/com/digginroom/digginroom/fixture/RoomFixture.kt
+++ android/app/src/test/java/com/digginroom/digginroom/fixture/RoomFixture.kt
@@ -10,9 +10,9 @@ object RoomFixture {
         videoId = "Jsm1qk2avyw",
         isScrapped = true,
         track = Track(
-            title = "",
-            artist = "",
-            superGenre = Genre("")
+            title = "예시 제목1",
+            artist = "예시 아티스트1",
+            superGenre = Genre("예시 장르1")
         )
     )
 
@@ -21,54 +21,54 @@ object RoomFixture {
             videoId = "Jsm1qk2avyw1",
             isScrapped = true,
             track = Track(
-                title = "",
-                artist = "",
-                superGenre = Genre("")
+                title = "예시 제목2",
+                artist = "예시 아티스트2",
+                superGenre = Genre("예시 장르2")
             )
         ),
         Room(
             videoId = "Jsm1qk2avyw2",
             isScrapped = true,
             track = Track(
-                title = "",
-                artist = "",
-                superGenre = Genre("")
+                title = "예시 제목3",
+                artist = "예시 아티스트3",
+                superGenre = Genre("예시 장르3")
             )
         ),
         Room(
             videoId = "Jsm1qk2avyw3",
             isScrapped = true,
             track = Track(
-                title = "",
-                artist = "",
-                superGenre = Genre("")
+                title = "예시 제목4",
+                artist = "예시 아티스트4",
+                superGenre = Genre("예시 장르4")
             )
         ),
         Room(
             videoId = "Jsm1qk2avyw4",
             isScrapped = true,
             track = Track(
-                title = "",
-                artist = "",
-                superGenre = Genre("")
+                title = "예시 제목5",
+                artist = "예시 아티스트5",
+                superGenre = Genre("예시 장르5")
             )
         ),
         Room(
             videoId = "Jsm1qk2avyw5",
             isScrapped = true,
             track = Track(
-                title = "",
-                artist = "",
-                superGenre = Genre("")
+                title = "예시 제목6",
+                artist = "예시 아티스트6",
+                superGenre = Genre("예시 장르6")
             )
         ),
         Room(
             videoId = "Jsm1qk2avyw6",
             isScrapped = true,
             track = Track(
-                title = "",
-                artist = "",
-                superGenre = Genre("")
+                title = "예시 제목7",
+                artist = "예시 아티스트7",
+                superGenre = Genre("예시 장르7")
             )
         )
     )
