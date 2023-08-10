c370df963efd8fe4f1be24ef86bda4a869d80c36
0chil <0@chll.it>
2023-08-10 11:26:14 +0900

    refactor: 사용하지 않는 코드 제거

--- backend/src/main/java/com/digginroom/digginroom/domain/Provider.java
+++ backend/src/main/java/com/digginroom/digginroom/domain/Provider.java
@@ -1,17 +1,7 @@
 package com.digginroom.digginroom.domain;
 
 public enum Provider {
-
-    SELF("{self}"),
-    GOOGLE("{google}");
-
-    private final String identifier;
-
-    Provider(final String identifier) {
-        this.identifier = identifier;
-    }
-
-    public String getIdentifier() {
-        return identifier;
-    }
+    
+    SELF,
+    GOOGLE;
 }
