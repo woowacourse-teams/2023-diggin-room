3fb95175cf8cafe1159b632536c9af6aa6924e92
whk06061 <gju06061@naver.com>
2023-08-11 01:24:32 +0900

    feat: RecyclerView 구조 작성

---
+++ android/app/src/main/java/com/digginroom/digginroom/feature/room/customview/roominfoview/adapter/CommentAdapter.kt
@@ -0,0 +1,19 @@
+package com.digginroom.digginroom.feature.room.customview.roominfoview.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.digginroom.digginroom.model.CommentModel
+
+class CommentAdapter : ListAdapter<CommentModel, CommentViewHolder>(CommentDiffUtilCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
+        return CommentViewHolder.of(parent)
+    }
+
+    override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    fun updateItems(items: List<CommentModel>) {
+        submitList(items)
+    }
+}
---
+++ android/app/src/main/java/com/digginroom/digginroom/feature/room/customview/roominfoview/adapter/CommentViewHolder.kt
@@ -0,0 +1,21 @@
+package com.digginroom.digginroom.feature.room.customview.roominfoview.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.digginroom.digginroom.databinding.ItemCommentBinding
+import com.digginroom.digginroom.model.CommentModel
+
+class CommentViewHolder(private val binding: ItemCommentBinding) : ViewHolder(binding.root) {
+    fun bind(item: CommentModel) {
+        binding.comment = item
+    }
+
+    companion object {
+        fun of(parent: ViewGroup): CommentViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemCommentBinding.inflate(layoutInflater)
+            return CommentViewHolder(binding)
+        }
+    }
+}
