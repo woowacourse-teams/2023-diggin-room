ff451f44d041d298cba784227d2e0bac8dbf0d80
songusika <sws981202@gmail.com>
2023-08-16 19:44:50 +0900

    refactor: 코드 컨벤션 적용

--- backend/src/main/java/com/digginroom/digginroom/controller/dto/CommentResponse.java
+++ backend/src/main/java/com/digginroom/digginroom/controller/dto/CommentResponse.java
@@ -3,8 +3,13 @@ package com.digginroom.digginroom.controller.dto;
 import com.digginroom.digginroom.domain.Comment;
 import java.time.LocalDateTime;
 
-public record CommentResponse(Long id, String writer, String comment, LocalDateTime createdAt,
-                              LocalDateTime updatedAt) {
+public record CommentResponse(
+        Long id,
+        String writer,
+        String comment,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
 
     public static CommentResponse of(final Comment comment) {
         return new CommentResponse(
