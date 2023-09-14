16446e8b260024c2edfd7de55bca8dae00fd7613
woogi <kimjinuk1999@gmail.com>
2023-09-14 23:54:06 +0900

    refactor: 테스트 코드를 추가 및 수정한다.

--- android/app/src/test/java/com/digginroom/digginroom/feature/login/LoginViewModelTest.kt
+++ android/app/src/test/java/com/digginroom/digginroom/feature/login/LoginViewModelTest.kt
@@ -63,7 +63,7 @@ class LoginViewModelTest {
         )
 
         // then
-        assertEquals(LoginUiState.Failed, loginViewModel.uiState.value)
+        assertEquals(LoginUiState.Failed, loginViewModel.uiState.getValue())
     }
 
     @Test
@@ -85,7 +85,7 @@ class LoginViewModelTest {
         )
 
         // then
-        assertEquals(LoginUiState.Succeed.Surveyed, loginViewModel.uiState.value)
+        assertEquals(LoginUiState.Succeed.Surveyed, loginViewModel.uiState.getValue())
     }
 
     @Test
@@ -107,34 +107,56 @@ class LoginViewModelTest {
         )
 
         // then
-        assertEquals(LoginUiState.Succeed.NotSurveyed, loginViewModel.uiState.value)
+        assertEquals(LoginUiState.Succeed.NotSurveyed, loginViewModel.uiState.getValue())
     }
 
     @Test
-    fun `id 토큰을 이용한 로그인 실패시 로그인 실패 상태가 된다`() {
+    fun `구글 로그인 진행 상태가 된다`() {
+        // when
+        loginViewModel.startGoogleLogin()
+        val actual = loginViewModel.uiState.getValue()
+
+        // then
+        val expected = LoginUiState.InProgress.GoogleLogin
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `id 토큰을 이용한 구글 로그인 실패시 로그인 실패 상태가 된다`() {
         // given
         coEvery {
             accountRepository.postLogin(ID_TOKEN)
         } returns LogResult.failure()
 
         // when
-        loginViewModel.login(ID_TOKEN)
+        loginViewModel.googleLogin(ID_TOKEN)
 
         // then
-        assertEquals(LoginUiState.Failed, loginViewModel.uiState.value)
+        assertEquals(LoginUiState.Failed, loginViewModel.uiState.getValue())
     }
 
     @Test
-    fun `id 토큰을 이용한 로그인 성공시 로그인 성공 상태가 된다`() {
+    fun `id 토큰을 이용한 구글 로그인 성공시 로그인 성공 상태가 된다`() {
         // given
         coEvery {
             accountRepository.postLogin(ID_TOKEN)
         } returns LogResult.success(Member(true))
 
         // when
-        loginViewModel.login(ID_TOKEN)
+        loginViewModel.googleLogin(ID_TOKEN)
 
         // then
-        assertEquals(LoginUiState.Succeed.Surveyed, loginViewModel.uiState.value)
+        assertEquals(LoginUiState.Succeed.Surveyed, loginViewModel.uiState.getValue())
+    }
+
+    @Test
+    fun `카카오 로그인 진행 상태가 된다`() {
+        // when
+        loginViewModel.startKakaoLogin()
+        val actual = loginViewModel.uiState.getValue()
+
+        // then
+        val expected = LoginUiState.InProgress.KaKaoLogin
+        assertEquals(expected, actual)
     }
 }
