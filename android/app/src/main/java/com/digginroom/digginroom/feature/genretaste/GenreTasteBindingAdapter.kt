6602bae7f2461779162cfa2125bcab812d032ff3
woogi <kimjinuk1999@gmail.com>
2023-09-14 14:20:43 +0900

    refactor: 사용하지 않는 함수들을 삭제한다.

--- android/app/src/main/java/com/digginroom/digginroom/feature/genretaste/GenreTasteBindingAdapter.kt
+++ android/app/src/main/java/com/digginroom/digginroom/feature/genretaste/GenreTasteBindingAdapter.kt
@@ -2,23 +2,12 @@ package com.digginroom.digginroom.feature.genretaste
 
 import android.graphics.Color
 import android.graphics.PorterDuff
-import android.view.View
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.digginroom.digginroom.feature.ResultListener
-import com.digginroom.digginroom.feature.genretaste.adpater.GenreTasteAdapter
-import com.digginroom.digginroom.model.GenreTasteModel
 
 object GenreTasteBindingAdapter {
-
-    @JvmStatic
-    @BindingAdapter("app:items")
-    fun setItems(recyclerView: RecyclerView, items: List<GenreTasteModel>?) {
-        (recyclerView.adapter as GenreTasteAdapter).submitList(items?.toMutableList())
-    }
 
     @JvmStatic
     @BindingAdapter("app:src")
@@ -31,23 +20,6 @@ object GenreTasteBindingAdapter {
             .load(imageResId)
             .centerCrop()
             .into(imageView)
-    }
-
-    @JvmStatic
-    @BindingAdapter(
-        value = ["surveyStateListener", "surveyState"],
-        requireAll = false
-    )
-    fun onClickForSurveyState(
-        view: View,
-        listener: ResultListener,
-        surveyState: GenreTasteSurveyState
-    ) {
-        when (surveyState) {
-            GenreTasteSurveyState.SUCCEED -> listener.onSucceed()
-            GenreTasteSurveyState.FAILED -> listener.onFailed()
-            else -> {}
-        }
     }
 
     @JvmStatic
