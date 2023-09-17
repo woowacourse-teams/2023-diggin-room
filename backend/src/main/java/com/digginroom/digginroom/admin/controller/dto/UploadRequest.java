83c600468aaca8b20e22d5a1e965268ed96fa2d0
Songusika <74398096+Songusika@users.noreply.github.com>
2023-09-17 19:03:23 +0900

    refactor: 룸 업로드 페이지 리팩터링 (#307)

--- backend/src/main/java/com/digginroom/digginroom/admin/controller/dto/UploadRequest.java
+++ backend/src/main/java/com/digginroom/digginroom/admin/controller/dto/UploadRequest.java
@@ -3,13 +3,14 @@ package com.digginroom.digginroom.admin.controller.dto;
 import com.digginroom.digginroom.domain.Genre;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UploadRequest(
-        @NotBlank String title,
-        @NotBlank String artist,
+        @NotBlank @Size(min = 1, max = 255) String title,
+        @NotBlank @Size(min = 1, max = 255) String artist,
         @NotNull Genre superGenre,
-        String description,
-        String subGenre,
-        @NotBlank String youtubeVideoId
+        @Size(max = 500) String description,
+        @Size(max = 255) String subGenre,
+        @NotBlank @Size(min = 1, max = 255) String youtubeVideoId
 ) {
 }
