7b4145f62a35eb11e19407126cadac8a2067a1a4
whk06061 <gju06061@naver.com>
2023-10-01 18:43:32 +0900

    feat: BottomSheetDialogFragment 하단에 댓글 작성 영역 고정되도록 구현

--- android/app/src/main/java/com/digginroom/digginroom/feature/room/comment/dialog/CommentDialog.kt
+++ android/app/src/main/java/com/digginroom/digginroom/feature/room/comment/dialog/CommentDialog.kt
@@ -2,14 +2,18 @@ package com.digginroom.digginroom.feature.room.comment.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.FrameLayout
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import com.digginroom.digginroom.R
+import com.digginroom.digginroom.databinding.DialogCommentEditTextBinding
 import com.digginroom.digginroom.databinding.DialogCommentLayoutBinding
 import com.digginroom.digginroom.feature.room.comment.CommentViewModel
 import com.digginroom.digginroom.feature.room.comment.adapter.CommentAdapter
@@ -17,7 +21,6 @@ import com.digginroom.digginroom.feature.room.comment.uistate.CommentMenuUiState
 import com.digginroom.digginroom.feature.room.comment.uistate.state.CommentPostState
 import com.digginroom.digginroom.model.CommentModel
 import com.dygames.androiddi.ViewModelDependencyInjector.injectViewModel
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -32,7 +35,9 @@ class CommentDialog : BottomSheetDialogFragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         binding = DialogCommentLayoutBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
@@ -43,6 +48,39 @@ class CommentDialog : BottomSheetDialogFragment() {
         return binding.root
     }
 
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        bottomSheetDialog.setOnShowListener {
+            val containerLayout =
+                dialog?.findViewById<FrameLayout>(com.google.android.material.R.id.container)
+
+            val editTextBinding = DialogCommentEditTextBinding.inflate(LayoutInflater.from(context))
+            val layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                Gravity.BOTTOM
+            )
+            editTextBinding.currentComment = binding.currentComment
+            editTextBinding.postComment = binding.postComment
+            editTextBinding.roomId = binding.roomId
+            containerLayout?.addView(editTextBinding.root, layoutParams)
+            editTextBinding.root.viewTreeObserver.addOnGlobalLayoutListener(object :
+                    ViewTreeObserver.OnGlobalLayoutListener {
+                    override fun onGlobalLayout() {
+                        editTextBinding.root.viewTreeObserver.removeOnGlobalLayoutListener(this)
+
+                        val height = editTextBinding.root.measuredHeight
+                        binding.root.setPadding(0, 0, 0, height ?: 0)
+                    }
+                })
+
+            editTextBinding.root.setOnClickListener {
+                Log.i("here22", "blah blah!!!!")
+            }
+        }
+        return bottomSheetDialog
+    }
+
     fun show(fragmentManager: FragmentManager, id: Long) {
         showNow(fragmentManager, "CommentDialog")
         binding.commentViewModel?.findComments(id)
@@ -51,7 +89,8 @@ class CommentDialog : BottomSheetDialogFragment() {
 
     private fun makeViewModel(): CommentViewModel {
         return ViewModelProvider(
-            this, injectViewModel<CommentViewModel>()
+            this,
+            injectViewModel<CommentViewModel>()
         )[CommentViewModel::class.java]
     }
 
@@ -60,7 +99,9 @@ class CommentDialog : BottomSheetDialogFragment() {
             CommentPostState.Post -> postComment(roomId, currentComment)
 
             is CommentPostState.Update -> updateComment(
-                roomId, commentPostState.commentId, currentComment
+                roomId,
+                commentPostState.commentId,
+                currentComment
             )
         }
         binding.currentComment = ""
@@ -68,13 +109,16 @@ class CommentDialog : BottomSheetDialogFragment() {
 
     private fun postComment(roomId: Long, currentComment: String) {
         binding.commentViewModel?.postComment(
-            roomId, currentComment
+            roomId,
+            currentComment
         )
     }
 
     private fun updateComment(roomId: Long, commentId: Long, currentComment: String) {
         binding.commentViewModel?.updateComment(
-            roomId, commentId, currentComment
+            roomId,
+            commentId,
+            currentComment
         )
         commentPostState = CommentPostState.Post
     }
@@ -84,8 +128,8 @@ class CommentDialog : BottomSheetDialogFragment() {
             CommentMenuUiState(update = {
                 updateCommentPostState(comment)
             }, delete = {
-                deleteComment(comment)
-            })
+                    deleteComment(comment)
+                })
         ).show(parentFragmentManager, "CommentMenuDialog")
     }
 
@@ -97,7 +141,8 @@ class CommentDialog : BottomSheetDialogFragment() {
     private fun deleteComment(comment: CommentModel) {
         binding.roomId?.let {
             binding.commentViewModel?.deleteComment(
-                it, comment.id
+                it,
+                comment.id
             )
         }
         binding.currentComment = ""
