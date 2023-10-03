4b7d64183df5e0d3b6e8ee533f0e4633f41220d6
whk06061 <gju06061@naver.com>
2023-10-04 06:12:24 +0900

    feat: 다이얼로그의 접힘, 펴짐에 상관없는 하단 고정 뷰 구현

--- android/app/src/main/java/com/digginroom/digginroom/feature/room/comment/adapter/CommentAdapter.kt
+++ android/app/src/main/java/com/digginroom/digginroom/feature/room/comment/adapter/CommentAdapter.kt
@@ -1,5 +1,6 @@
 package com.digginroom.digginroom.feature.room.comment.adapter
 
+import android.util.Log
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.digginroom.digginroom.model.CommentModel
@@ -8,6 +9,7 @@ class CommentAdapter(private val menuClickListener: (CommentModel) -> Unit) :
     ListAdapter<CommentModel, CommentViewHolder>(CommentDiffUtilCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
+        Log.d("HKHK", "onCreateViewHolder: ")
         return CommentViewHolder.of(parent) {
             menuClickListener(getItem(it))
         }
