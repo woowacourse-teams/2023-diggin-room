c844dd993726396934e4bdc558cf7bb3df652c93
0chil <0@chll.it>
2023-10-05 22:48:33 +0900

    chore: migration 스크립트, index 이름

--- backend/src/main/java/com/digginroom/digginroom/feedback/domain/Feedback.java
+++ backend/src/main/java/com/digginroom/digginroom/feedback/domain/Feedback.java
@@ -3,6 +3,8 @@ package com.digginroom.digginroom.feedback.domain;
 import com.digginroom.digginroom.domain.BaseEntity;
 import com.digginroom.digginroom.domain.member.Member;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 public class Feedback extends BaseEntity {
 
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_feedback_writer"))
     private Member writer;
     private String content;
 
--- backend/src/main/resources/db/migration/V6.2__create_table_feedback.sql
+++ backend/src/main/resources/db/migration/V6.2__create_table_feedback.sql
@@ -3,5 +3,8 @@ create table feedback
     created_at datetime(6)  null,
     id         bigint auto_increment primary key,
     updated_at datetime(6)  null,
-    content    varchar(255) null
-)
+    writer_id  bigint       null,
+    content    varchar(255) null,
+    constraint fk_feedback_writer
+        foreign key (writer_id) references member (id)
+);
