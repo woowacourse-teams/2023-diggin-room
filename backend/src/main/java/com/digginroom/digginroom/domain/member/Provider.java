d46d91cedc8895816035d4d6a854b63045e5ec08
thdwoqor <celebarte01@gmail.com>
2023-10-06 21:30:24 +0900

    feat: Provider issueUrl 제거

--- backend/src/main/java/com/digginroom/digginroom/domain/member/Provider.java
+++ backend/src/main/java/com/digginroom/digginroom/domain/member/Provider.java
@@ -1,30 +1,8 @@
 package com.digginroom.digginroom.domain.member;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 public enum Provider {
 
-    SELF(""),
-    GOOGLE("https://accounts.google.com"),
-    KAKAO("https://kauth.kakao.com");
-
-    private final String issueUrl;
-
-    Provider(final String issueUrl) {
-        this.issueUrl = issueUrl;
-    }
-
-    public static Optional<Provider> of(final String issuer) {
-        return Arrays.stream(values())
-                .filter(it -> it.matches(issuer))
-                .findFirst();
-    }
-
-    private boolean matches(final String issueUrl) {
-        if (this.issueUrl.isEmpty()) {
-            return false;
-        }
-        return this.issueUrl.equals(issueUrl);
-    }
+    SELF,
+    GOOGLE,
+    KAKAO;
 }
