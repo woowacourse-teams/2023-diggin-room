a5ab023d63c711d55a97fb2d4b1369a4e1dd5785
0chil <0@chll.it>
2023-10-10 11:04:49 +0900

    refactor: migration script 컬럼 순서

--- backend/src/main/resources/db/migration/V6.2__create_table_feedback.sql
+++ backend/src/main/resources/db/migration/V6.2__create_table_feedback.sql
@@ -1,7 +1,7 @@
 create table feedback
 (
-    created_at datetime(6)  null,
     id         bigint auto_increment primary key,
+    created_at datetime(6)  null,
     updated_at datetime(6)  null,
     writer_id  bigint       null,
     content    varchar(255) null,
