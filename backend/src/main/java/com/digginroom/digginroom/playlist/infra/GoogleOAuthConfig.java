ab2dfa253e9d3404bc50bfd5e32f3be20918fe1a
thdwoqor <celebarte01@gmail.com>
2023-10-13 14:21:16 +0900

    refactor: @Data 대신 record 사용

--- backend/src/main/java/com/digginroom/digginroom/playlist/infra/GoogleOAuthConfig.java
+++ backend/src/main/java/com/digginroom/digginroom/playlist/infra/GoogleOAuthConfig.java
@@ -1,14 +1,12 @@
 package com.digginroom.digginroom.playlist.infra;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Data
 @ConfigurationProperties(prefix = "oauth.google")
-public class GoogleOAuthConfig {
-
-    private String clientId;
-    private String clientSecret;
-    private String redirectUri;
-    private String grantType;
+public record GoogleOAuthConfig(
+        String clientId,
+        String clientSecret,
+        String redirectUri,
+        String grantType
+) {
 }
