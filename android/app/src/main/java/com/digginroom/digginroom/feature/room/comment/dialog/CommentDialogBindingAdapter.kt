26ffa1244984f49ab28ba543e911b2bd92878dd7
whk06061 <gju06061@naver.com>
2023-10-13 17:22:42 +0900

    feat: 댓글 전송 시 리사이클러뷰 자동 스크롤 및 수정 시 커서 위치 조정

--- android/app/src/main/java/com/digginroom/digginroom/feature/room/comment/dialog/CommentDialogBindingAdapter.kt
+++ android/app/src/main/java/com/digginroom/digginroom/feature/room/comment/dialog/CommentDialogBindingAdapter.kt
@@ -1,22 +1,26 @@
 package com.digginroom.digginroom.feature.room.comment.dialog
 
+import android.text.Editable
+import android.text.Selection
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.digginroom.digginroom.feature.room.comment.adapter.CommentAdapter
 import com.digginroom.digginroom.feature.room.comment.uistate.CommentResponseUiState
 
-@BindingAdapter("app:comments")
-fun RecyclerView.comments(state: CommentResponseUiState?) {
-    state ?: return
-    when (state) {
+@BindingAdapter("app:responseState")
+fun RecyclerView.comments(responseState: CommentResponseUiState?) {
+    responseState ?: return
+    when (responseState) {
         is CommentResponseUiState.Failed -> Unit
         CommentResponseUiState.Loading -> Unit
         is CommentResponseUiState.Succeed -> {
             (adapter as? CommentAdapter)?.submitList(
-                state.comments
+                responseState.comments
             )
+            smoothScrollToPosition(0)
         }
     }
 }
@@ -51,4 +55,10 @@ fun TextView.visible(state: CommentResponseUiState?) {
             }
         }
     }
+}
+
+@BindingAdapter("app:cursorIndex")
+fun EditText.cursorIndex(comments: String) {
+    val editable: Editable = this.text
+    Selection.setSelection(editable, editable.length)
 }
