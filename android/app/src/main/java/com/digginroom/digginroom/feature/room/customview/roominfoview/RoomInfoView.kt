f8e502f1dbdb995f8bd4367325e3c1581f257dd4
whk06061 <gju06061@naver.com>
2023-10-15 22:39:21 +0900

    feat: 튜토리얼 화면에서 RoomInfo Layout 재사용되도록 구현

---
+++ android/app/src/main/java/com/digginroom/digginroom/feature/room/customview/roominfoview/RoomInfoView.kt
@@ -0,0 +1,27 @@
+package com.digginroom.digginroom.feature.room.customview.roominfoview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.digginroom.digginroom.databinding.ItemRoomInfoBinding
+import com.digginroom.digginroom.feature.room.roominfo.RoomInfoUiState
+import com.digginroom.digginroom.model.mapper.ScrapCountFormatter
+
+class RoomInfoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) :
+    ConstraintLayout(context, attrs) {
+
+    private val binding: ItemRoomInfoBinding =
+        ItemRoomInfoBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+        binding.scrapCountFormatter = ScrapCountFormatter
+    }
+
+    fun updateRoomInfoUiState(roomInfoUiState: RoomInfoUiState) {
+        binding.roomInfoUiState = roomInfoUiState
+    }
+}
