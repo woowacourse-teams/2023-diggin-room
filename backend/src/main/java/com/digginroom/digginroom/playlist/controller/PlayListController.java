be4e6aecc3c3dbb03b99a9842205d9281037ec70
Jae-Baek Song <83541246+thdwoqor@users.noreply.github.com>
2023-10-16 13:57:49 +0900

    refactor: @Controller->@RestController 변경 및 endpoint 수정 (#376)

--- backend/src/main/java/com/digginroom/digginroom/playlist/controller/PlayListController.java
+++ backend/src/main/java/com/digginroom/digginroom/playlist/controller/PlayListController.java
@@ -5,17 +5,17 @@ import com.digginroom.digginroom.playlist.service.dto.PlayListRequest;
 import com.digginroom.digginroom.playlist.service.dto.PlayListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class PlayListController {
 
     private final PlayListService playListService;
 
-    @PostMapping("/playList")
+    @PostMapping("/playlist")
     public ResponseEntity<PlayListResponse> makePlayList(@RequestBody PlayListRequest playListRequest) {
         return ResponseEntity.ok().body(playListService.makePlayList(playListRequest));
     }
