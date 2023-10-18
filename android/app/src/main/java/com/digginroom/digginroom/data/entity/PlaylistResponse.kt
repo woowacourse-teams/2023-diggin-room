998e01ed4ff8f38b16ea2e705b1dcb2bedeb1e6f
boogi-woogi <kimjinuk1999@gmail.com>
2023-10-18 14:37:47 +0900

    fix: release 버전에서 플레이리스트 추출이 되지 않는 버그를 수정한다. (#403)

--- android/app/src/main/java/com/digginroom/digginroom/data/entity/PlaylistResponse.kt
+++ android/app/src/main/java/com/digginroom/digginroom/data/entity/PlaylistResponse.kt
@@ -1,8 +1,10 @@
 package com.digginroom.digginroom.data.entity
 
+import androidx.annotation.Keep
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+@Keep
 @Serializable
 data class PlaylistResponse(
     @SerialName("count")
