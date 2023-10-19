2699ea4f11ed7bd61eb1d5f4155bf94e9bf11e9e
Doyeop Kim <dygames34@gmail.com>
2023-10-19 18:16:54 +0900

    feat: Android 버전 1.2.2로 업그레이드

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.digginroom.digginroom"
         minSdk = 28
         targetSdk = 33
-        versionCode = 5
-        versionName = "1.2.1"
+        versionCode = 6
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArgument(
