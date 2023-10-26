92521bb6aebb452ef48d456552c001d5dcdf2bbb
whk06061 <gju06061@naver.com>
2023-10-26 14:28:54 +0900

    feat: CommentItem 추상화

--- android/app/src/main/java/com/digginroom/digginroom/model/CommentModel.kt
+++ android/app/src/main/java/com/digginroom/digginroom/model/CommentModel.kt
@@ -1,9 +1,14 @@
 package com.digginroom.digginroom.model
 
-data class CommentModel(
-    val id: Long,
-    val writer: String,
-    val comment: String,
-    val elapsedTime: String,
-    val isOwner: Boolean
-)
+sealed class CommentItem {
+    data class CommentModel(
+        val id: Long,
+        val writer: String,
+        val comment: String,
+        val elapsedTime: String,
+        val isOwner: Boolean,
+        val infiniteLoading: String = ""
+    ) : CommentItem()
+
+    object Loading : CommentItem()
+}
