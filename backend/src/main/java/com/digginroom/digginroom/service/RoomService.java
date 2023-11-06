350504c0e6e81f6524d2a044449c36422bb61257
songusika <sws981202@gmail.com>
2023-11-06 15:45:07 +0900

    chore: 후행 작업 merge

--- backend/src/main/java/com/digginroom/digginroom/service/RoomService.java
+++ backend/src/main/java/com/digginroom/digginroom/service/RoomService.java
@@ -1,18 +1,11 @@
 package com.digginroom.digginroom.service;
 
-import com.digginroom.digginroom.domain.Genre;
 import com.digginroom.digginroom.domain.member.Member;
 import com.digginroom.digginroom.domain.recommend.RoomRecommender;
 import com.digginroom.digginroom.domain.room.Room;
-import com.digginroom.digginroom.repository.MemberRepository;
 import com.digginroom.digginroom.exception.RecommendException;
 import com.digginroom.digginroom.repository.MemberRepository;
 import com.digginroom.digginroom.repository.RoomRepository;
-import com.digginroom.digginroom.service.dto.RoomResponse;
-import com.digginroom.digginroom.service.dto.RoomsResponse;
-import com.digginroom.digginroom.service.dto.TrackResponse;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 import com.digginroom.digginroom.service.dto.RoomResponse;
 import com.digginroom.digginroom.service.dto.RoomsResponse;
 import com.digginroom.digginroom.service.dto.TrackResponse;
@@ -28,15 +21,13 @@ public class RoomService {
     private final RoomRepository roomRepository;
     private final MemberRepository memberRepository;
     private final RoomRecommender roomRecommender;
-    private final MemberRepository memberRepository;
 
     @Transactional(readOnly = true)
     public RoomResponse recommend(final Long memberId) {
         Member member = memberRepository.getMemberById(memberId);
 
         try {
-            Genre recommendedGenre = new GenreRecommender().recommend(member.getMemberGenres());
-            Room recommendedRoom = recommendRoom(recommendedGenre);
+            Room recommendedRoom = roomRecommender.recommend(member);
 
             return new RoomResponse(
                     recommendedRoom.getId(),
@@ -88,41 +79,5 @@ public class RoomService {
         Member member = memberRepository.getMemberById(memberId);
 
         member.undislike(room);
-    }
-
-    public CommentsResponse findRoomComments(final Long roomId, final Long loginMemberId) {
-        validateExistRoom(roomId);
-        Member member = memberService.findMember(loginMemberId);
-        return commentService.getRoomComments(roomId, member);
-    }
-
-    public void validateExistRoom(final Long roomId) {
-        if (!roomRepository.existsById(roomId)) {
-            throw new NotFoundException(roomId);
-        }
-    }
-
-    public CommentResponse comment(final Long roomId, final Long memberId, final CommentRequest request) {
-        validateExistRoom(roomId);
-        Member member = memberService.findMember(memberId);
-        return commentService.comment(roomId, member, request);
-    }
-
-    public void deleteComment(final Long roomId, final Long memberId, final Long commentId) {
-        validateExistRoom(roomId);
-        Member member = memberService.findMember(memberId);
-        commentService.delete(roomId, member, commentId);
-    }
-
-    public CommentResponse updateComment(
-            final Long roomId,
-            final Long memberId,
-            final Long commentId,
-            final CommentRequest request
-    ) {
-        validateExistRoom(roomId);
-        Member member = memberService.findMember(memberId);
-        Comment updateComment = commentService.update(member, roomId, commentId, request);
-        return CommentResponse.of(updateComment, updateComment.isOwner(member));
     }
 }
