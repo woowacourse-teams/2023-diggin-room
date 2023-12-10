fc5acb1313c393247a1d49a9859a5c62506fa9df
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-12-10 23:02:19 +0900

    chore: 컨벤션 통일

--- backend/src/main/java/com/digginroom/digginroom/feedback/controller/FeedbackController.java
+++ backend/src/main/java/com/digginroom/digginroom/feedback/controller/FeedbackController.java
@@ -1,7 +1,6 @@
 package com.digginroom.digginroom.feedback.controller;
 
 import com.digginroom.digginroom.controller.Auth;
-import com.digginroom.digginroom.domain.member.Member;
 import com.digginroom.digginroom.feedback.FeedbackService;
 import com.digginroom.digginroom.feedback.dto.FeedbackRequest;
 import com.digginroom.digginroom.feedback.dto.FeedbackResponse;
@@ -12,12 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/feedbacks")
 public class FeedbackController {
 
     private final FeedbackService feedbackService;
@@ -31,7 +30,7 @@ public class FeedbackController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/feedbacks")
+    @GetMapping
     public ResponseEntity<List<FeedbackResponse>> showFeedbacks() {
         return ResponseEntity.ok().body(feedbackService.getAll());
     }
