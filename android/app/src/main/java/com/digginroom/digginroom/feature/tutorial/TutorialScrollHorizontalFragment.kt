4b32e78da9ece6030000c0c40ef44a7e9de73720
Hyegyeong <gju06061@naver.com>
2023-12-17 08:15:19 +0900

    feat: 좌우 스크롤 튜토리얼 구현

--- android/app/src/main/java/com/digginroom/digginroom/feature/tutorial/TutorialScrollHorizontalFragment.kt
+++ android/app/src/main/java/com/digginroom/digginroom/feature/tutorial/TutorialScrollHorizontalFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.digginroom.digginroom.R
 class TutorialScrollHorizontalFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
