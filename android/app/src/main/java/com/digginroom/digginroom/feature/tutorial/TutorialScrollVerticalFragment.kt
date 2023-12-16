e2c8944ea8d07e6210bb9af8dd784811b6c2b235
Hyegyeong <gju06061@naver.com>
2023-12-17 08:14:11 +0900

    feat: 상하 스크롤 튜토리얼 구현

--- android/app/src/main/java/com/digginroom/digginroom/feature/tutorial/TutorialScrollVerticalFragment.kt
+++ android/app/src/main/java/com/digginroom/digginroom/feature/tutorial/TutorialScrollVerticalFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.digginroom.digginroom.R
-
 class TutorialScrollVerticalFragment : Fragment() {
 
     override fun onCreateView(
