8760bed7794243d27e4f227330702f8469dc3d99
Kong-hana01 <gksqlsl11@khu.ac.kr>
2024-01-24 14:27:19 +0900

    refactor: AuthDatabaseCleaner 구현

--- backend/src/test/java/com/digginroom/digginroom/util/AuthDatabaseCleaner.java
+++ backend/src/test/java/com/digginroom/digginroom/util/AuthDatabaseCleaner.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ public class AuthDatabaseCleaner implements DatabaseCleaner {
     @Autowired
     private EntityManager entityManager;
     private List<String> tableNames;
+    private final Set<DatabaseTableName> filterDatabaseTableNames = Set.of(DatabaseTableName.MEMBER); // 외부로 빼기
 
     @Override
     @PostConstruct
@@ -23,7 +25,10 @@ public class AuthDatabaseCleaner implements DatabaseCleaner {
     }
 
     private List<String> getTableNames() {
-        return getTableMetaData().stream().map(this::getTableName).toList();
+        return getTableMetaData().stream()
+                .map(this::getTableName)
+                .filter(tableName -> filterDatabaseTableNames.contains(DatabaseTableName.of(tableName)))
+                .toList();
     }
 
     @SuppressWarnings("unchecked")
---
+++ backend/src/test/java/com/digginroom/digginroom/util/DatabaseTableName.java
@@ -0,0 +1,26 @@
+package com.digginroom.digginroom.util;
+
+import java.util.Arrays;
+
+public enum DatabaseTableName {
+
+    MEMBER("member"),
+    EMPTY("");
+
+    private final String name;
+
+    DatabaseTableName(final String name) {
+        this.name = name;
+    }
+
+    public static DatabaseTableName of(final String name) {
+        return Arrays.stream(values())
+                .filter(databaseTableName -> databaseTableName.getName().equals(name))
+                .findFirst()
+                .orElse(EMPTY);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
